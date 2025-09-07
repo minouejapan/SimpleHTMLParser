dd1dcfc702cf07aa8d4b8dad4041918ebe46edf8
INOUE, masahiro <30848557+minouejapan@users.noreply.github.com>
2025-09-07 16:48:55 +0900

    Add files via upload

--- sample/na6dl2.dpr
+++ sample/na6dl2.dpr
@@ -1,35 +1,42 @@
 ﻿(*
-  TSHParserを使用したなろう系ダウンローダーサンプル
+  TSHParserを使用したなろう系作品テキストダウンローダー
+  na6dl2.exe (HTML解析をSHParserで刷新したためna6dl2と命名)
+  LazarusにてWindows版とLinux版をビルド可能(Windows版はDelphiでもビルド可能かも)
+  必要なライブラリ：
+    SHParser:https://github.com/minouejapan/SimpleHTMLParser
+    TRegExpr:https://github.com/andgineer/TRegExpr
 
+  ver1.0  2025/09/07 SHParserサンプルに対してna6dlと同等機能を実装した
 
-  ver1.1  2025/09/02 大見出しをうまく抽出出来なかった不具合を修正した
-          Linuxでも動作できるようにした
-  ver1.0  2025/08/25 SHParser使用例として作成
 *)
 program na6dl2;
 
 {$IFDEF FPC}
-  {$MODE Delphi}
-  {$codepage utf8}
+  {$MODE DELPHI}
+  {$CODEPAGE UTF8}
 {$ELSE}
   {$APPTYPE CONSOLE}
 {$ENDIF}
 
 uses
-  Classes, SysUtils, RegExpr,
-{$IFDEF LINUX}
-  UniHtml,
-{$ELSE}
-  WinHTML,
-{$ENDIF}
-  HParse in '..\hparse.pas',
-  SHParser in '..\shparser.pas'
-{$IFNDEF FPC}
-  , LazUTF8Wrap
-{$ELSE}
-  , LazUTF8
-{$ENDIF}
-  ;
+  Classes, SysUtils, RegExpr, Windows,
+  {$IFDEF LINUX}
+    UniHtml, // FpHTTPClient
+  {$ELSE}
+    WinHTML, // WinINet
+  {$ENDIF}
+    HParse,
+    SHParser
+  {$IFNDEF FPC}
+    ,LazUTF8Wrap
+  {$ELSE}
+    ,LazUTF8
+  {$ENDIF}
+    ;
+
+{$R *.res}
+{$R na6dl2ver.res}
+
 
 type
   TNvStat = record
@@ -42,22 +49,27 @@ type
   end;
 
 const
-  VERSION = 'na6dl2 ver1.1 2025/09/02 INOUE, masahiro';
+  VERSION = 'na6dl2 ver1.0 2025/09/07 INOUE, masahiro';
 {$IFDEF LINUX}
   CRLF = #10;
 {$ELSE}
   CRLF = #13#10;
 {$ENDIF}
+  // ユーザメッセージID
+  WM_DLINFO  = WM_USER + 30;
 
 var
   TextBuff, LogFile: TStringList;
-  FileName, LogName: string;
+  FileName, LogName, StartPage: string;
   CpTitle: string;
   CookieName,
   CookieData: string;
+  hWnd: THandle;
+  StartN: integer;
 
 
 // なろう系青空文庫準拠形式エンコード
+// TSHParserのOnBeforeGetTextから呼ばれる
 function AozoraDecord(Src: string): string;
 var
   tmp: string;
@@ -84,8 +96,8 @@ begin
   tmp := UTF8StringReplace(tmp, '">挿絵</a>', '）入る］' + CRLF, [rfReplaceAll]);
   Result := tmp;
 end;
-//-------------------------------------------
 
+// 作品情報取得
 function GetNvStat(Src: string): TNvStat;
 var
   aurl, res, sn: string;
@@ -96,22 +108,22 @@ begin
   Result.AuthURL := '';
   Parser := TSHParser.Create(Src);
   try
-    aurl := Parser.GetRegExText('<a class="c-menu__item c-menu__item--headnav" href="', '">作品情報</a>', False);
+    aurl := Parser.FindRegex('<a class="c-menu__item c-menu__item--headnav" href="', '">作品情報</a>', False);
   finally
     Parser.Free;
   end;
   res := GetHTML(aurl, CookieName, CookieData);
   Parser := TSHParser.Create(res);
   try
-    Result.NvlStat := Parser.GetRegExText('<span class="p-infotop-type__type.*?">', '</span>', False);
-    Result.AuthURL := Parser.GetRegExText('<dd class="p-infotop-data__value"><a href="', '">', False);
-    Result.FstDate := Parser.GetRegExText('<dt class="p-infotop-data__title">掲載日</dt>.*?">', '</dd>', False);
-    Result.FnlDate := Parser.GetRegExText('<dt class="p-infotop-data__title">最終掲載日</dt>.*?">', '</dd>', False);
-    Result.LstDate := Parser.GetRegExText('<dt class="p-infotop-data__title">最新掲載日</dt>.*?">', '</dd>', False);
+    Result.NvlStat := Parser.FindRegex('<span class="p-infotop-type__type.*?">', '</span>', False);
+    Result.AuthURL := Parser.FindRegex('<dd class="p-infotop-data__value"><a href="', '">', False);
+    Result.FstDate := Parser.FindRegex('<dt class="p-infotop-data__title">掲載日</dt>.*?">', '</dd>', False);
+    Result.FnlDate := Parser.FindRegex('<dt class="p-infotop-data__title">最終掲載日</dt>.*?">', '</dd>', False);
+    Result.LstDate := Parser.FindRegex('<dt class="p-infotop-data__title">最新掲載日</dt>.*?">', '</dd>', False);
 {$IFDEF FPC}
-    sn := SysToUTF8(Parser.GetNodeText('span', 'class', 'p-infotop-type__allep', False));
+    sn := SysToUTF8(Parser.Find('span', 'class', 'p-infotop-type__allep', False));
 {$ELSE}
-    sn := Parser.GetNodeText('span', 'class', 'p-infotop-type__allep', False);
+    sn := Parser.Find('span', 'class', 'p-infotop-type__allep', False);
 {$ENDIF}
     sn := ReplaceRegExpr('全', ReplaceRegExpr('エピソード', sn, ''), '');
     sn := StringReplace(sn, ',', '', [rfReplaceAll]);
@@ -178,18 +190,18 @@ begin
     // テキスト化の前処理を登録する
     Parser.OnBeforeGetText := @AozoraDecord;
 
-    res := Parser.GetNodeText('h1', 'class', 'p-novel__title p-novel__title--rensai');
+    res := Parser.Find('h1', 'class', 'p-novel__title p-novel__title--rensai');
     if res <> '' then
       txt := txt + '［＃中見出し］' + res + '［＃中見出し終わり］'+ CRLF;
     // 前書き
-    res := Parser.GetNodeText('div', 'class', 'js-novel-text p-novel__text p-novel__text--preface');
+    res := Parser.Find('div', 'class', 'js-novel-text p-novel__text p-novel__text--preface');
     if res <> '' then
       txt := txt + '［＃ここから罫囲み］' + CRLF + res + CRLF + '［＃ここで罫囲み終わり］' + CRLF + '［＃水平線］' + CRLF;
     // 本文
-    res := Parser.GetNodeText('div', 'class', 'js-novel-text p-novel__text');
+    res := Parser.Find('div', 'class', 'js-novel-text p-novel__text');
     txt := txt + res + CRLF;
     // 後書き
-    res := Parser.GetNodeText('div', 'class', 'js-novel-text p-novel__text p-novel__text--afterword');
+    res := Parser.Find('div', 'class', 'js-novel-text p-novel__text p-novel__text--afterword');
     if res <> '' then
       txt := txt + '［＃水平線］' + CRLF + '［＃ここから罫囲み］' + CRLF + res + CRLF + '［＃ここで罫囲み終わり］' + CRLF;
     // ページ終わり
@@ -202,12 +214,19 @@ end;
 
 procedure NarouDL(URLAddr: string);
 var
-  res, aurl, txt, title, author, st: string;
+  res, aurl, txt, title, author, st, sendstr: string;
   stat: TNvStat;
   i: integer;
   Parser: TSHParser;
   r: TRegExpr;
   isShort: boolean;
+{$IFDEF WINDOWS}
+{$IFDEF FPC}
+  ws: WideString;
+{$ENDIF}
+  CDS: TCopyDataStruct;
+  conhdl: THandle;
+{$ENDIF}
 begin
   res := GetHTML(URLAddr, CookieName, CookieData);
   // トップページ
@@ -218,17 +237,21 @@ begin
   try
     // テキスト化の前処理を登録する
     Parser.OnBeforeGetText := @AozoraDecord;
-    title := Parser.GetNodeText('h1', 'class', 'p-novel__title');
+    title := Parser.Find('h1', 'class', 'p-novel__title');
     // ファイル名を準備する
-    FileName := Parser.PathFilter(title);
-    LogName  := st + FileName + '.log';
-    FileName := st + FileName + '.txt';
-
+    if FileName = '' then
+    begin
+      FileName := Parser.PathFilter(title);
+      LogName  := st + FileName + '.log';
+      FileName := st + FileName + '.txt';
+    end else begin
+      LogName := ChangeFileExt(FileName, '.log');
+    end;
     TextBuff.Add(st + Title);
-    author := Parser.GetNodeText('div', 'class', 'p-novel__author', False);
+    author := Parser.Find('div', 'class', 'p-novel__author', False);
     author := ReplaceRegExpr('<.*?>', ReplaceRegExpr('作者：', author, ''), '');
     TextBuff.Add(author);
-    txt := Parser.GetNodeText('div', 'class', 'p-novel__summary');
+    txt := Parser.Find('div', 'class', 'p-novel__summary');
     if txt <> '' then
       TextBuff.Add('［＃ここから罫囲み］' + CRLF + txt + CRLF + '［＃ここで罫囲み終わり］' + CRLF + '［＃改ページ］')
     else
@@ -245,6 +268,24 @@ begin
     LogFile.Add('あらすじ');
     LogFile.Add(txt + CRLF);
     LogFile.Add(DateToStr(Now));
+{$IFDEF WINDOWS}
+    // Naro2mobiから呼び出された場合は進捗状況をSendする
+    if hWnd <> 0 then
+    begin
+      conhdl := GetStdHandle(STD_OUTPUT_HANDLE);
+      sendstr := title + ',' + author;
+      Cds.dwData := Stat.TotalPg - StartN + 1;
+    {$IFDEF FPC}
+      ws := UTF8ToUTF16(sendstr);
+      Cds.cbData := ByteLength(ws) + 2;
+      Cds.lpData := PWideChar(ws);
+    {$ELSE}
+      Cds.cbData := (UTF8Length(sendstr) + 1) * SizeOf(Char);
+      Cds.lpData := Pointer(sendstr);
+    {$ENDIF}
+      SendMessage(hWnd, WM_COPYDATA, conhdl, LPARAM(Addr(Cds)));
+{$ENDIF}
+    end;
   finally
     Parser.Free;
   end;
@@ -269,7 +310,7 @@ begin
   Write('各話を取得中 [  0/' + Format('%3d', [stat.TotalPg]) + ']');
   CpTitle := '';  // 章タイトル検出用
   // 各話を取得する
-  for i := 1 to stat.TotalPg do
+  for i := StartN to stat.TotalPg do
   begin
     Write(#13'各話を取得中 [' + Format('%3d', [i]) + '/' + Format('%3d', [stat.TotalPg]) +']');
     aurl := URLAddr + IntToStr(i) + '/';
@@ -285,13 +326,20 @@ begin
     end;
     txt := GetBody(res); // 本文を取得する
     TextBuff.Add(txt);
+{$IFDEF WINDOWS}
+    if hWnd <> 0 then
+      SendMessage(hWnd, WM_DLINFO, i - StartN + 1, 1);
+{$ENDIF}
     Sleep(500);
   end;
   Writeln(CRLF+ ' ... ' + IntToStr(stat.TotalPg) + ' 個のエピソードを取得しました.');
 end;
 
 var
-  aurl: string;
+  aurl, op: string;
+  i: integer;
+
+
 begin
   if ParamCount = 0 then
   begin
@@ -299,10 +347,52 @@ begin
     Writeln(VERSION);
     Writeln('  使用方法');
     Writeln('  na6dl2 小説トップページURL');
+    ExitCode := -1;
     Exit;
   end;
+  StartN := 1;
+  ExitCode := 0;
+  // オプション引数取得
+  for i := 0 to ParamCount - 1 do
+  begin
+    op := ParamStr(i + 1);
+    // Naro2mobiのWindowsハンドル
+    if UTF8Pos('-h', op) = 1 then
+    begin
+      UTF8Delete(op, 1, 2);
+      try
+        hWnd := StrToInt(op);
+      except
+        Writeln('Error: Invalid Naro2mobi Handle.');
+        ExitCode := -1;
+        Exit;
+      end;
+    // DL開始ページ番号
+    end else if UTF8Pos('-s', op) = 1 then
+    begin
+      UTF8Delete(op, 1, 2);
+      StartPage := op;
+      try
+        StartN := StrToInt(op);
+      except
+        Writeln('Error: Invalid Start Page Number.');
+        ExitCode := -1;
+        Exit;
+      end;
+    // 作品URL
+    end else if UTF8Pos('https:', op) = 1 then
+    begin
+      aurl := op;
+      if UTF8Copy(aurl, UTF8Length(aurl), 1) <> '/' then
+        aurl := aurl + '/';
+    // それ以外であれば保存ファイル名
+    end else begin
+      FileName := op;
+      if UTF8UpperCase(ExtractFileExt(op)) <> '.TXT' then
+        FileName := FileName + '.txt';
+    end;
+  end;
 
-  aurl := ParamStr(1);
   if (UTF8Pos('https://ncode.syosetu.com/n', aurl) <> 1) and (UTF8Pos('https://novel18.syosetu.com/n', aurl) <> 1) then
   begin
     Writeln('小説のURLが違います.');
